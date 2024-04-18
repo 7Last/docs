6bb8d50e7254b6a555fde007db07857fffda8073
elenaferr0 <elena.ferro@gmail.com>
2024-04-18 18:33:43 +0200

    minor fixes to check glossary

--- scripts/check_glossary.py
+++ scripts/check_glossary.py
@@ -1,35 +1,49 @@
 import re, os
 
-glossary_tex = '2_RTB/documentazione_interna/glossario/glossario.tex'
-root_folder = '../'
-glossary_url = 'https://7last.github.io/docs/rtb/glossario'
+root_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+glossary_tex = f'{root_folder}/2_RTB/documentazione_interna/glossario/glossario.tex'
+glossary_url = 'https://7last.github.io/docs/rtb/documentazione-interna/glossario'
 
-def parse_glossary(glossary_tex):
-    with open(root_folder + glossary_tex, 'r') as f:
-        file = f.read()
+
+def parse_glossary():
+    file = open(glossary_tex, 'r').read()
     file = re.sub(r'[\n\t\r]', '', file)
-    rows = re.findall(r'\\newglossaryentry{(.*?)}{\s*name={(.*?)},\s*description={(.*?)},\s*plural={(.*?)},\s*feminine={(.*?)},\s*feminine_plural={(.*?)}', file)
+
+    name = r'(?=.*name=(?:{?)([^,}]+)(?:}?))'
+    description = r'(?=.*description=(?:{?)([^,}]+)(?:}?))'
+    plural = '(?:.*plural=(?:{?)([^,}]+)(?:}?))?'
+    feminine = '(?:.*plural=(?:{?)([^,}]+)(?:}?))?'
+    feminine_plural = '(?:.*feminine_plural=(?:{?)([^,}]+)(?:}?))?'
+
+    pattern = r'\\newglossaryentry{([^{}]+)}' + fr'{name}{description}{plural}{feminine}{feminine_plural}'
+    matches = re.findall(pattern, file)
+
     glossary = {}
-    ordered_glossary = {}
-    for word, name, _, plural, feminine, feminine_plural in rows:
+    for word, name, _, plural, feminine, feminine_plural in matches:
         glossary[word] = {
             'name': name,
             'plural': plural,
             'feminine': feminine,
-            'feminine_plural': feminine_plural}
+            'feminine_plural': feminine_plural,
+        }
+
+    ordered_glossary = {}
     for k in sorted(glossary, key=len, reverse=True):
         ordered_glossary[k] = glossary[k]
     return ordered_glossary
 
-def search_tex_files(root_folder):
-    glossary = parse_glossary(glossary_tex)
+
+def search_tex_files():
+    exclude = ['glossario', '0_template', '1_candidatura', 'variables.tex', 'title.tex', 'header.tex', 'packages.tex']
+    glossary = parse_glossary()
     for root, _, files in os.walk(root_folder):
         for file in files:
             path = os.path.join(root, file)
-            if "glossario" in path or "0_template" in path or "1_candidatura" in path or file == "variables.tex" or file == "title.tex" or file == "header.tex" or file == "packages.tex":
+            if any([e in path for e in exclude]):
                 continue
             if file.endswith('.tex'):
-                replace_word(path, glossary)
+                replace_words(path, glossary)
+
 
 def is_within_section(file_content, start):
     matches = [m for m in re.finditer(r'section{[^}]*', file_content[:start])]
@@ -39,13 +53,15 @@ def is_within_section(file_content, start):
             return True
     return False
 
+
 def is_already_subscripted(file_content, end):
     matches = [m for m in re.finditer(r'[^}]*\\textsubscript{G}', file_content[end:])]
     for match in matches:
         match_start = match.start()
-        if '}' not in file_content[end:end+match_start]:
+        if '}' not in file_content[end:end + match_start]:
             return True
     return False
+
 
 def is_within_href(file_content, start):
     matches = [m for m in re.finditer(r'\\href{[^}]*', file_content[:start])]
@@ -55,29 +71,31 @@ def is_within_href(file_content, start):
             return True
     return False
 
+
 def style_command(file_content, start):
-    style_commands = ['\\textit{', '\\itshape{', '\\emph{', '\\textbf{', '\\bfseries{', '\\uline{', '\\underline{', '\\textsc{', '\\scshape{']
+    style_commands = ['\\textit{', '\\itshape{', '\\emph{', '\\textbf{', '\\bfseries{', '\\uline{', '\\underline{',
+                      '\\textsc{', '\\scshape{']
     for command in style_commands:
-        if command in file_content[max(0, start-len(command)):start]:
+        if command in file_content[max(0, start - len(command)):start]:
             return command
     return ''
 
-def replace_word(path, glossary):
+
+def replace_words(path, glossary):
     with open(path, 'r') as f:
         file_content = f.read()
         if not glossary:
             print("Empty glossary. Exiting...")
             return
+
     for word in glossary:
         file_content_lower = file_content.lower()
-        parole = [glossary[word]['name'],
-                  glossary[word]['plural'],
-                  glossary[word]['feminine'],
-                  glossary[word]['feminine_plural']]
-        for parola in parole:
-            if parola == '' or parola.lower() not in file_content_lower:
+        word_variants = glossary[word].values()
+
+        for variant in word_variants:
+            if variant == '' or variant.lower() not in file_content_lower:
                 continue
-            pattern = r'\b' + re.escape(parola) + r'\b'
+            pattern = r'\b' + re.escape(variant) + r'\b'
             matches = [m for m in re.finditer(pattern, file_content, re.IGNORECASE)]
             for match in reversed(matches):
                 match_start = match.start()
@@ -89,16 +107,21 @@ def replace_word(path, glossary):
                         match_end += 1
                     else:
                         break
-                if is_within_href(file_content, match_start) or is_within_section(file_content, match_start) or is_already_subscripted(file_content, match_end):
+
+                if is_within_href(file_content, match_start) \
+                        or is_within_section(file_content, match_start) \
+                        or is_already_subscripted(file_content, match_end):
                     continue
                 url = glossary_url + "#" + word.lower().replace(' ', '-')
                 replacement = '\\href{' + url + '}{' + file_content[match_start:match_end] + '\\textsubscript{G}}'
                 file_content = file_content[:match_start] + replacement + file_content[match_end:]
-    with open(path, 'w') as f:
-        f.write(file_content)
+
+    open(path, 'w').write(file_content)
+
 
 def main():
-    search_tex_files(root_folder)
+    search_tex_files()
+
 
 if __name__ == '__main__':
     main()
